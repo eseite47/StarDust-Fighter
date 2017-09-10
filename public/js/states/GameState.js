ed3973a3a31ff318bbffe414e34f88be0c62b157
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-09-10 14:47:48 -0400

    Improved Gameplay: faster hero ship, meteors, and no more gameOver when invaders get off screen

--- public/js/states/GameState.js
+++ public/js/states/GameState.js
@@ -6,7 +6,7 @@ var GameState = {
     let self = this;
     this.cursors = this.game.input.keyboard.createCursorKeys();
     this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'background');
-    this.background.autoScroll(0, 20);
+    this.background.autoScroll(0, 30);
     this.game.world.setBounds(0, 0, 1400, 750)
 
     const title = game.add.text(this.game.width/2, 50, 'STARDUST FIGHTER', {fill: '#00FF00', font: '30px Press Start 2P'})
@@ -36,20 +36,32 @@ var GameState = {
     this.game.physics.arcade.enable(this.hero);
     this.hero.body.allowGravity = false;
     this.hero.body.collideWorldBounds = true;
-    this.SPEED = 250;
+    this.SPEED = 350;
 
+    //lasers
     this.lasers = this.add.group();
     const spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
     spaceBar.onDown.add(function(){
       this.createPew(this.hero.world.x, this.hero.world.y)
     }, this)
+
+    //Meteors
+    this.meteors = this.add.group();
+    const meteorFrequency = 10;
+    this.meteorCreator = this.game.time.events.loop(Phaser.Timer.SECOND * meteorFrequency, this.createMeteor, this);
   },
 
   update: function(){
-    //console.log(this.gameOver)
+
     this.game.physics.arcade.collide(this.lasers, this.invaders, this.scoreUp);
 
     this.game.physics.arcade.collide(this.hero, this.invaders, this.gameOver);
+
+    this.game.physics.arcade.collide(this.hero, this.meteors, this.gameOver);
+
+    this.game.physics.arcade.collide(this.lasers, this.meteors, this.meteorImpact);
+
+    this.game.physics.arcade.collide(this.invaders, this.meteors, this.meteorImpact);
 
     this.hero.body.velocity.x = 0;
     this.hero.body.velocity.y = 0;
@@ -75,24 +87,30 @@ var GameState = {
 
     this.invaders.forEach(function(element){
       if (element.y > 750){
-        this.gameOver()
+        //this.gameOver()
+        element.kill();
+      }
+    }, this)
+
+    this.meteors.forEach(function(element){
+      if (element.y > 750){
+        element.kill();
       }
     }, this)
   },
 
-  randomX: function(){
+  randomStartPoint: function(){
     return Math.floor(Math.random()*1200) +100;
   },
 
   createInvader: function(){
-    const x = this.randomX()
+    const x = this.randomStartPoint()
     let invader = this.invaders.getFirstExists(false)
     if (!invader){
       invader = this.invaders.create(0, 0, 'ship');
       invader.anchor.setTo(0.5, 0);
       invader.animations.add('animate', [0, 1], 1.5, true);
       invader.play('animate')
-      // invader.customParams = {sound: self.game.add.audio('shipSound'), points: 10};
       this.game.physics.arcade.enable(invader);
       invader.body.allowGravity = false;
     }
@@ -105,30 +123,81 @@ var GameState = {
     if(!pew){
       pew = this.lasers.create(0, 0, 'pew')
       pew.anchor.setTo(0.5, 1);
-      //pew.inputEnabled = true;
-      //pew.input.enableDrag();
-      game.physics.arcade.enable(pew);
+      this.game.physics.arcade.enable(pew);
       pew.body.allowGravity = false;
     }
     pew.reset(xCoord, yCoord -75);
     pew.body.velocity.y = -100
   },
 
+  createMeteor: function(){
+    const coordX = this.randomStartPoint();
+    let meteor = this.meteors.getFirstExists(false)
+    if (!meteor){
+      const meteorSprite = this.getMeteorSprite()()
+      meteor = this.meteors.create(0, 0, meteorSprite)
+      meteor.anchor.setTo(0.5)
+      this.game.physics.arcade.enable(meteor);
+      meteor.body.allowGravity = false;
+      meteor.body.immovable = true;
+    }
+    var meteorRotation = this.game.add.tween(meteor);
+    meteorRotation.to({angle: '-3600'}, 25000)
+    meteorRotation.start()
+    //meteor.angle += 1;
+    meteor.reset(coordX, 100);
+    meteor.body.velocity.y = 30;
+  },
+
   refreshScore: function(){
     this.publicScore.text = this.hero.customParams.score
   },
 
-  scoreUp: function(pew, invader, hero){
+  scoreUp: function(pew, invader){
     //this.hero.customParams.score += 10;
     pew.kill();
     invader.kill();
     // this.refreshScore();
   },
 
+  meteorImpact: function(element, meteor){
+    element.kill();
+  },
+
   gameOver: function(){
     //this.game.state.restart();
     console.log('this.state', this.state)
     game.state.start('HomeState', true, false, 'GAME OVER')
+  },
+
+  getMeteorSprite: function(){
+    let counter = 0;
+    return function(){
+      console.log('counter in returned function', counter)
+      if (counter >= 3){
+        counter = 0
+      }
+      else if (counter === 0){
+        counter++
+        console.log('meteor 1', counter)
+        return 'meteor'
+      }
+      else if (counter === 1){
+        console.log('meteor 2')
+        counter++
+        return 'meteor2'
+      }
+      else if (counter === 2){
+        console.log('meteor 3')
+        counter++
+        return 'meteor3'
+      }
+      else{
+        console.log('else')
+        counter++
+        return 'meteor'
+      }
+    }
   }
 
 };
--- public/js/states/PreloadState.js
+++ public/js/states/PreloadState.js
@@ -8,14 +8,14 @@ var PreloadState = {
     this.load.spritesheet('ship', './assets/img/invaders.png', 34, 26, 2);
     this.load.image('hero', './assets/img/hero2c.png', 72, 72, 4);
     this.load.image('pew', './assets/img/beams.png')
-
+    this.load.image('meteor', './assets/img/meteor.png')
+    this.load.image('meteor2', './assets/img/meteor2.png')
+    this.load.image('meteor3', './assets/img/meteor3.png')
     this.load.audio('shipSound', ['./assets/audio/shipMoves.ogg', './assets/audio/shipMoves.mp3']);
-
     this.load.audio('heroShootSounds', ['./assets/audio/pewpew.ogg', './assets/audio/pewpew.mp3']);
-
     this.load.audio('heroMoveSounds', ['./assets/audio/heroChangeDirection.ogg', './assets/audio/heroChangeDirection.mp3'])
   },
   create: function(){
-    this.state.start('HomeState')
+    setTimeout(this.state.start('HomeState', 5000))
   }
 }
